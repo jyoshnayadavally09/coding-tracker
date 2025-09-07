8a90bdea8bc35e96f873d3dab2d33613a2f55c20
jyoshnayadavally09 <yadavallyjyoshna2006@email.com>
2025-09-07 07:52:45 +0530

    Removed broken submodule src/components/lost

--- src/components/lostandfound[1]/project-root/backend/fetchData.js
+++
@@ -1,5 +0,0 @@
-async function getLeetCodeStats(username) {
-    const res = await fetch(`https://leetcode-stats-api.herokuapp.com/${username}`);
-    const data = await res.json();
-    return data.totalSolved || 0;
-}
